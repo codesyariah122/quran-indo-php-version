1c772fb7085b8b51ad32c9f58b1371c36cda3181
codesyariah122 <pujiermanto@gmail.com>
2024-11-09 13:38:33 +0700

    update/headers

--- app/controllers/QuranController.php
+++ app/controllers/QuranController.php
@@ -12,7 +12,6 @@ class QuranController extends Controller {
     public function __construct() {
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-        header("Access-Control-Allow-Headers: Content-Type, Authorization");
         header('Content-Type: application/json');
         header('Accept: application/json');
 
--- public/index.php
+++ public/index.php
@@ -6,11 +6,14 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header('Content-Type: application/json');
 header('Accept: application/json');
 // Cek apakah permintaan adalah OPTIONS dan respon dengan status 200 untuk preflight
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+	header('Content-Type: application/json');
+	header('Accept: application/json');
 	http_response_code(200);
 	exit;
 }
