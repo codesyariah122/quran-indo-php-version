442544ececaf97d205f340b9fe1ae0d7c148bdb1
codesyariah122 <pujiermanto@gmail.com>
2024-11-09 13:05:45 +0700

    update/headers

--- app/controllers/QuranController.php
+++ app/controllers/QuranController.php
@@ -10,9 +10,12 @@ use Dotenv\Dotenv;
 
 class QuranController extends Controller {
     public function __construct() {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
         header('Content-Type: application/json');
         header('Accept: application/json');
-        
+
         $dotenv = Dotenv::createImmutable('../');
         $dotenv->load();
     }
--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header('Content-Type: application/json');
 header('Accept: application/json');
 // Cek apakah permintaan adalah OPTIONS dan respon dengan status 200 untuk preflight
