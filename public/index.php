48a35a4dcd521b92361346ab03eb97850c2cf781
codesyariah122 <pujiermanto@gmail.com>
2024-11-09 12:02:44 +0700

    update/header

--- public/index.php
+++ public/index.php
@@ -4,6 +4,16 @@
  * @return callback
  * */
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Cek apakah permintaan adalah OPTIONS dan respon dengan status 200 untuk preflight
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+	http_response_code(200);
+	exit;
+}
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
